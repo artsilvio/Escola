f92e0b607b23b9b8af31b627260a2dd7aaee073c
Silvio <Silvio@notenooksilvionov>
2026-03-18 22:33:19 -0300

    Adicionado Joaquim

--- src/br/com/escola/app/Main.java
+++ src/br/com/escola/app/Main.java
@@ -10,7 +10,7 @@ public class Main {
         AlunoService.criarAluno("João", 18, 123);
         AlunoService.criarAluno("Maria", -10, 456);
         AlunoService.criarAluno("Carlos", 25, 789);
-
+        AlunoService.criarAluno("Joaquim", 19, 779);
     }
 
 }
