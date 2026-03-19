b861a40396a81131591fe56ff4a41e8e83888078
Silvio <Silvio@notenooksilvionov>
2026-03-18 22:52:47 -0300

    Cometarios de impressão

--- src/br/com/escola/app/Main.java
+++ src/br/com/escola/app/Main.java
@@ -6,7 +6,7 @@ import br.com.escola.service.AlunoService;
 public class Main {
 
     public static void main(String[] args) {
-
+        //Impressão da lista de alunos
         AlunoService.criarAluno("João", 18, 123);
         AlunoService.criarAluno("Maria", -10, 456);
         AlunoService.criarAluno("Carlos", 25, 789);
